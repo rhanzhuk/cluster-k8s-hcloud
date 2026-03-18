3462b18e5edd73c74ef3a90db4051e58317063fe
rhanzhuk <hanzhuk_ruslan@icloud.com>
2026-03-18 10:49:02 +0100

    set workers to dynamics count

--- terraform/k8s-nodes.tf
+++ terraform/k8s-nodes.tf
@@ -50,8 +50,16 @@ resource "hcloud_server" "master-3" {
   }
 }
 
-resource "hcloud_server" "worker-1" {
-  name        = "worker-1"
+variable "worker_count" {
+  description = "Number of workers node"
+  default     = 2
+  type        = number
+
+}
+
+resource "hcloud_server" "workers" {
+  count       = var.worker_count
+  name        = "worker-${count.index + 1}"
   image       = "ubuntu-24.04"
   server_type = "cpx31"
   location    = "hil"
@@ -63,23 +71,43 @@ resource "hcloud_server" "worker-1" {
   }
   network {
     network_id = hcloud_network.main.id
-    ip         = local.worker_1_priv_ip
+    ip         = "10.0.10.${20 + count.index}"
+
   }
 }
 
-resource "hcloud_server" "worker-2" {
-  name        = "worker-2"
-  image       = "ubuntu-24.04"
-  server_type = "cpx31"
-  location    = "hil"
-  ssh_keys    = [hcloud_ssh_key.pub_key.id]
-  user_data   = file("./cloud-init/cloud-init-server.yaml")
-  public_net {
-    ipv4_enabled = false
-    ipv6_enabled = false
-  }
-  network {
-    network_id = hcloud_network.main.id
-    ip         = local.worker_2_priv_ip
-  }
-}
+
+
+#resource "hcloud_server" "worker-1" {
+#  name        = "worker-1"
+#  image       = "ubuntu-24.04"
+#  server_type = "cpx31"
+#  location    = "hil"
+#  ssh_keys    = [hcloud_ssh_key.pub_key.id]
+#  user_data   = file("./cloud-init/cloud-init-server.yaml")
+#  public_net {
+#    ipv4_enabled = false
+#    ipv6_enabled = false
+#  }
+#  network {
+#    network_id = hcloud_network.main.id
+#    ip         = local.worker_1_priv_ip
+#  }
+#}
+
+#resource "hcloud_server" "worker-2" {
+#  name        = "worker-2"
+#  image       = "ubuntu-24.04"
+#  server_type = "cpx31"
+#  location    = "hil"
+#  ssh_keys    = [hcloud_ssh_key.pub_key.id]
+#  user_data   = file("./cloud-init/cloud-init-server.yaml")
+#  public_net {
+#    ipv4_enabled = false
+#    ipv6_enabled = false
+#  }
+#  network {
+#    network_id = hcloud_network.main.id
+#    ip         = local.worker_2_priv_ip
+#  }
+#}
--- terraform/main.tf
+++ terraform/main.tf
@@ -19,14 +19,38 @@ provider "hcloud" {
 }
 
 locals {
+  masters = [
+    {
+      name = "master-1"
+      ip   = local.master_1_priv_ip
+    },
+    {
+      name = "master-2"
+      ip   = local.master_2_priv_ip
+    },
+    {
+      name = "master-3"
+      ip   = local.master_3_priv_ip
+    }
+  ]
+
+  workers = [
+    for i, srv in hcloud_server.workers : {
+      name = "worker-${i + 1}"
+      ip   = tolist(srv.network)[0].ip
+    }
+  ]
+}
+
+locals {
   master_1_priv_ip = "10.0.10.10"
   master_2_priv_ip = "10.0.10.11"
   master_3_priv_ip = "10.0.10.12"
-  worker_1_priv_ip = "10.0.10.20"
-  worker_2_priv_ip = "10.0.10.21"
-  nat_priv_ip      = "10.0.10.50"
-  k8s_api_priv_ip  = "10.0.10.40"
-  bastion_ip       = "10.0.10.55"
+  # worker_1_priv_ip = "10.0.10.20"
+  # worker_2_priv_ip = "10.0.10.21"
+  nat_priv_ip     = "10.0.10.50"
+  k8s_api_priv_ip = "10.0.10.40"
+  bastion_ip      = "10.0.10.55"
 }
 
 # ssh pub key
@@ -50,10 +74,11 @@ output "master_private_ip" {
 }
 
 output "workers_private_ip" {
-  value = {
-    worker_1 = local.worker_1_priv_ip
-    worker_2 = local.worker_2_priv_ip
-  }
+  value = [for srv in hcloud_server.workers : tolist(srv.network)[0].ip]
+}
+
+output "worker_names" {
+  value = [for i in range(var.worker_count) : "worker-${i + 1}"]
 }
 
 output "bastion_public_ip" {
@@ -68,15 +93,30 @@ resource "local_file" "ansible_ini" {
   filename = "${path.module}/../ansible/inventory.ini"
 
   content = templatefile("${path.module}/../ansible/inventory.ini.tmpl", {
-    bastion_ip       = hcloud_server.bastion.ipv4_address,
-    k8s_api_priv_ip  = local.k8s_api_priv_ip,
-    master_1_priv_ip = local.master_1_priv_ip,
-    master_2_priv_ip = local.master_2_priv_ip,
-    master_3_priv_ip = local.master_3_priv_ip,
-    worker_1_priv_ip = local.worker_1_priv_ip,
-    worker_2_priv_ip = local.worker_2_priv_ip
+    bastion_ip      = hcloud_server.bastion.ipv4_address,
+    k8s_api_priv_ip = local.k8s_api_priv_ip,
+    masters         = local.masters,
+    workers         = local.workers
   })
+
 }
+
+
+#resource "local_file" "ansible_ini" {
+#  filename = "${path.module}/../ansible/inventory.ini"
+
+#  content = templatefile("${path.module}/../ansible/inventory.ini.tmpl", {
+#    bastion_ip       = hcloud_server.bastion.ipv4_address,
+#    k8s_api_priv_ip  = local.k8s_api_priv_ip,
+#    master_1_priv_ip = local.master_1_priv_ip,
+#    master_2_priv_ip = local.master_2_priv_ip,
+#    master_3_priv_ip = local.master_3_priv_ip,
+#    worker_1_priv_ip = local.worker_1_priv_ip,
+#    worker_2_priv_ip = local.worker_2_priv_ip
+#  })
+#}
+
+
 
 resource "local_file" "ansible_playbook" {
   filename = "${path.module}/../ansible/playbooks/install-cluster-ha-k8s.yml"
